2edfc0effd37cf909aa0e06d41fb5a243de82427
Sam Johnsson <johnsson.sam@gmail.com>
2015-09-08 13:16:45 +0200

    just keep cleaning

--- www/ajax/adduser.php
+++ www/ajax/adduser.php
@@ -4,11 +4,11 @@ require_once('../../config.php');
 require_once(ROOT_PATH.'/includes.php');
 
     ?>
-    <div  class="popupHeader">Skapa persondata</div>
-<div><input id="create_user_firstname" class="form-control" type="text" placeholder="Förnamn"></div>
-<div><input id="create_user_lastname" class="form-control" type="text" placeholder="Efternamn"></div>
-<div><input id="create_user_personalid" class="form-control" type="text" placeholder="Personnummer(yyyymmdd-xxxx)"></div>
-<div><input id="create_user_employmentdate" class="form-control" type="date" placeholder="Datum (YYYY-MM-DD)"></div>
-<div>
-	<span class="glyphicon glyphicon-ok glyphIconBig" style="cursor:pointer" onclick="addUser()"></span>
-</div>
+	<div class="popupHeader">Skapa persondata</div>
+	<div><input id="create_user_firstname" class="form-control" type="text" placeholder="Förnamn"></div>
+	<div><input id="create_user_lastname" class="form-control" type="text" placeholder="Efternamn"></div>
+	<div><input id="create_user_personalid" class="form-control" type="text" placeholder="Personnummer(yyyymmdd-xxxx)"></div>
+	<div><input id="create_user_employmentdate" class="form-control" type="date" placeholder="Datum (YYYY-MM-DD)"></div>
+	<div>
+		<span class="glyphicon glyphicon-ok glyphIconBig" style="cursor:pointer" onclick="addUser()"></span>
+	</div>
