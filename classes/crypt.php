99927ae04f4463b1198d23328f782f578a310602
Sam Johnsson <johnsson.sam@gmail.com>
2015-09-08 13:14:55 +0200

    more cleaning

--- classes/crypt.php
+++ classes/crypt.php
@@ -1,8 +1,6 @@
 <?php 
 	class Crypt{
 
-		
-
 	    function simple_encrypt($text)
 	    {
 	        return trim(base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_256, SALT, $text, MCRYPT_MODE_ECB, mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB), MCRYPT_RAND))));
