e2ec4d53e275acbb09898ef1528a1322dcd51661
Sam Johnsson <johnsson.sam@gmail.com>
2015-09-08 13:18:15 +0200

    cleaning.. cleaning

--- classes/db.php
+++ classes/db.php
@@ -6,8 +6,8 @@
 			try {
 				$this->dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME.";charset=utf8", DB_USER, DB_PASS,
 				array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-				} catch(PDOException $e) {
-					echo $e->getMessage();
+			} catch(PDOException $e) {
+				echo $e->getMessage();
 			}
 		}
 
